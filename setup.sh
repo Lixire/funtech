684b4b5a8a1dda5af2901451eb7dba5b91b8883e
Anthony Zhang <azhang9@gmail.com>
2017-01-28 19:34:09 -0500

    Update stuff

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 pip install tweepy
+pip install flask
--- web/__init__.py
+++ web/__init__.py
@@ -3,27 +3,42 @@
 import re
 from datetime import datetime, date, timedelta
 import sqlite3
+from collections import defaultdict
 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, g
 
-# set up application
+DATABASE = "funtech.db"
+
 app = Flask(__name__, static_url_path="/static")
-conn = sqlite3.connect("funtech.db")
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 def get_top_sentiment_companies(conn):
-    company_scores = {}
-    today = date.today
+    company_scores = defaultdict(int)
+    today = date.today()
     date_weights = {today - timedelta(days=days_ago): 1.0 / 2 ** days_ago for days_ago in range(7 + 1)}
-    for tweet_date, company, average_sentiment in con.execute("select date, company, sum(sentiment) / count(sentiment) from measurements where date >= date('now', '-1 week') group by date, company"):
-        date_value = date(*tweet_date.split("-"))
+    for tweet_date, company, average_sentiment in conn.execute("select date, company, sum(sentiment) / count(sentiment) from sentiments where date >= date('now', '-7 days') group by date, company"):
+        date_value = datetime.strptime(tweet_date, "%Y-%m-%d").date()
         company_scores[company] += date_weights[date_value] * average_sentiment
-    companies = sorted(company_scores.items(), key=lambda e: -e[1])
-    return companies
+    min_sentiment, max_sentiment = min(company_scores.values()), max(company_scores.values())
+    if min_sentiment == max_sentiment: min_sentiment = max_sentiment - 1
+    company_scores = [
+        (company, (sentiment - min_sentiment) / (max_sentiment - min_sentiment))
+        for company, sentiment in company_scores.items()
+    ]
+    return sorted(company_scores, key=lambda e: -e[1]) * 100
 
 @app.route("/")
 def index():
-    top_sentiment_companies = get_top_sentiment_companies(conn)
-    return render_template('index.html', trending_companies = top_sentiment_companies)
+    top_sentiment_companies = get_top_sentiment_companies(get_db())
+    return render_template(
+        'index.html',
+        trending_companies=top_sentiment_companies,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True, port=8888) # debug mode
