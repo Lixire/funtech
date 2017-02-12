55f18d6fd7dfeda6dcada9b123123401e01df0be
Anthony Zhang <azhang9@gmail.com>
2017-02-12 15:54:25 -0500

    fix stuff

--- cleanup.py
+++ cleanup.py
@@ -2,11 +2,14 @@
 
 import sqlite3
 from datetime import date, timedelta
+from os import path
 
 # run `crontab -e` to edit the cron settings
 # edit it to have the following line at the end: `0 0 * * *    python /home/ubuntu/cleanup.py`
 
-conn = sqlite3.connect("funtech.db")
+DATABASE_FILE = path.join(path.dirname(path.abspath(__file__)), "funtech.db")
+
+conn = sqlite3.connect(DATABASE_FILE)
 
 conn.execute("CREATE TABLE IF NOT EXISTS sentiments (date TEXT, company TEXT, sentiment FLOAT)")
 
